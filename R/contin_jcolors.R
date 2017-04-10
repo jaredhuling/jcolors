f7999bba7444ee60125cb9666a4b9c02c815e00c
jaredhuling <huling@wisc.edu>
2017-04-10 13:10:47 -0500

    added reverse option

--- R/contin_jcolors.R
+++ R/contin_jcolors.R
@@ -4,6 +4,8 @@
 #' Creates different color palette functions
 #'
 #' @param palette Character string indicating a palette of colors.
+#' @param reverse logical value indicating whether the color palette should be reversed. Defaults
+#' to \code{FALSE}
 #' @param interpolate Character string for color interpolation method.
 #' "linear" or "spline" interpolation available
 #' @return returns a function that takes an integer argument (the required number of colors), which
@@ -20,15 +22,20 @@
 #'       xlab = "", ylab = "",
 #'       xaxt = "n", yaxt = "n", bty = "n")
 #'
-jcolors_contin <- function(palette = c("default"), interpolate = c("spline", "linear"))
+jcolors_contin <- function(palette = c("default"),
+                           reverse = FALSE,
+                           interpolate = c("spline", "linear"))
 {
 
     interpolate <- match.arg(interpolate)
+    reverse     <- as.logical(reverse)
 
     default <- c('yankees_blue'   = "#202547",
                  'purple_taupe'   = "#53354A",
                  'deep_taupe'     = "#7A6C5D",
                  'straw'          = "#E3D26F")
+
+    if (reverse) default <- rev(default)
 
     default.func <- colorRampPalette(default, interpolate = interpolate)
 
